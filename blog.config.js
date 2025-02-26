659247912f305bfd1b6d5a7e06835e29820b41de
misscraft <88709293+misscraft@users.noreply.github.com>
2025-02-26 11:26:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,10 +15,10 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Coma鸭', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '欢迎来我们的相册集。', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '我是coma，喜欢拍照。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://comaduck.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 播客, 相册集', // 网站关键词 英文逗号隔开
-  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 播客, 相册集, comaduck, Coma鸭， 摄影', // 网站关键词 英文逗号隔开
+  BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://pic1.imgdb.cn/item/67be89a0d0e0a243d405d898.png', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我们是一群摄影爱好者，欢迎来akaluchi.com看我们的相册集，顺便在未来推出实体相册。🎉',
+    'Hi，我们是一群摄影爱好者，欢迎来akaluchi.com看我们的相册集，顺便在未来推出实体相册🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
